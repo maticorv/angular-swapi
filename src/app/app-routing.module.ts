96d16a1848b588c49db183887e8f02b3221f77bf
Matias <matias_corvalan@outlook.com>
2018-05-29 01:45:51 -0300

    rutas refactorizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FilmsComponent } from './films/films.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { SpaceshipsComponent } from './spaceships/spaceships.component';
+import { SpeciesComponent } from './species/species.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'films', component: FilmsComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'planets', component: PlanetsComponent },
+  { path: 'spaceships', component: SpaceshipsComponent },
+  { path: 'species', component: SpeciesComponent },
+  { path: 'vehicle', component: VehiclesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
