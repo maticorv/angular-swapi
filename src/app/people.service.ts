97f23efbdf9f5382ca0100bb640a29b5ca82b7b7
Matias <matias_corvalan@outlook.com>
2018-05-29 10:56:44 -0300

    servicio refactorizado

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class PeopleService {
 
-  constructor() { }
+  private static url = 'https://swapi.co/api/people/';
 
+  constructor( private http: HttpClient) { }
+
+  /** GET people from the server */
+  getCustomers(): Observable<People[]> {
+    return this.http.get(PeopleService.url)
+        .map(data => {
+          return data['results'];
+        });
+  }
+  // Manejo de error
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+}
+// Was generated using the JSON to TS extension for JS
+export interface People {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
 }
