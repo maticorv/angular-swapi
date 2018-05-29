6b4e67ed3f2606611aa69bb266146075fceff2a0
Matias <matias_corvalan@outlook.com>
2018-05-29 01:34:35 -0300

    Commit inicial creado con todos los componentes necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { SpaceshipsComponent } from './spaceships/spaceships.component';
+import { SpeciesComponent } from './species/species.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent,
+    PeopleComponent,
+    PlanetsComponent,
+    SpaceshipsComponent,
+    SpeciesComponent,
+    VehiclesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
