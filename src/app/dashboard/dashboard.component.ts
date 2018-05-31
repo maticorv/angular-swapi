7a1150cbb101112650e357614c8ad6cef7bcd12e
Matias <matias_corvalan@outlook.com>
2018-05-30 21:47:09 -0300

    agregamos la seccion de personajes principales

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import {PeopleService } from '../people.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  people: any[];
 
-  constructor() { }
+  constructor(private peopleService: PeopleService) { }
 
   ngOnInit() {
+    this.peopleService
+    .getPeople()
+    .subscribe(data => {this.people = data.slice(0, 4); });
   }
 
 }
